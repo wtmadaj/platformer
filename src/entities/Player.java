49b8f04513117fced2a1fde1eb51dfc18a4f0f53
Wes Madaj <wtmadaj@gmail.com>
2024-05-13 11:21:28 -0400

    Code through episode 10 - added gravity, jumping, updated collision detection

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -7,7 +7,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 import static src.main.utils.Constants.PlayerConstants.*;
-import static src.main.utils.HelpMethods.CanMoveHere;
+import static src.main.utils.HelpMethods.*;
 
 public class Player extends Entity {
 
@@ -18,16 +18,23 @@ public class Player extends Entity {
     private int playerAction = IDLE;
     private boolean isMoving = false;
     private boolean attacking = false;
-    private boolean left, up, right, down;
+    private boolean left, up, right, down, jump;
     private float playerSpeed = 2.0f;
     private int[][] levelData;
     private float xDrawOffset = 21 * Game.SCALE, yDrawOffset = 4 * Game.SCALE;
+
+    //Jumping / gravity variables
+    private float airSpeed = 0f;
+    private float gravity = 0.04f*Game.SCALE;
+    private float jumpSpeed = -2.25f*Game.SCALE;
+    private float fallSpeedAfterCollision = 0.5f*Game.SCALE;
+    private boolean inAir = false;
 
 
     public Player(float x, float y, int width, int height) {
         super(x,y, width, height);
         loadAnimations();
-        initHitBox(x, y, 20*Game.SCALE, 28*Game.SCALE);
+        initHitBox(x, y, 20*Game.SCALE, 27*Game.SCALE);
     }
 
     public void update() {
@@ -38,7 +45,7 @@ public class Player extends Entity {
 
     public void render(Graphics g) {
         g.drawImage((Image) animations[playerAction][animationIndex], (int) (hitBox.x - xDrawOffset), (int) (hitBox.y - yDrawOffset), width, height, null);
-        drawHitBox(g); //For debugging hit box
+//        drawHitBox(g); /* For debugging hit box */
     }
 
     private void updateAnimationTick() {
@@ -61,6 +68,14 @@ public class Player extends Entity {
             playerAction = IDLE;
         }
 
+        if(inAir) {
+            if(airSpeed < 0) {
+                playerAction = JUMPING;
+            } else {
+                playerAction = FALLING;
+            }
+        }
+
         if(attacking) {
             playerAction = ATTACK_1;
         }
@@ -77,28 +92,62 @@ public class Player extends Entity {
 
     private void updatePosition() {
         isMoving = false;
-        if(!left && !right && !up && !down) return;
 
-        float xSpeed = 0, ySpeed = 0;
-
-        if(left && !right)
-            xSpeed = -playerSpeed;
-        else if(right && !left)
-            xSpeed = playerSpeed;;
-
-
-        if(up && !down)
-            ySpeed = -playerSpeed;
-        else if(down && !up)
-            ySpeed = playerSpeed;
-
-
-        if(CanMoveHere(hitBox.x + xSpeed, hitBox.y + ySpeed, hitBox.width, hitBox.height, levelData)) {
-            hitBox.x += xSpeed;
-            hitBox.y += ySpeed;
-            isMoving = true;
+        if(jump) {
+            jump();
+        }
+        if(!left && !right && !inAir) return;
+        if (!inAir) {
+            if(!IsEntityOnFloor(hitBox, levelData)) {
+                inAir = true;
+            }
         }
 
+        float xSpeed = 0;
+        if(left)
+            xSpeed -= playerSpeed;
+        if(right)
+            xSpeed += playerSpeed;
+
+        if(inAir) {
+            if(CanMoveHere(hitBox.x, hitBox.y + airSpeed, hitBox.width, hitBox.height, levelData)) {
+                hitBox.y += airSpeed;
+                airSpeed += gravity;
+                updateXPos(xSpeed);
+            } else {
+                hitBox.y = GetEntityYPosUnderRoofOrAboveFloor(hitBox, airSpeed);
+                if(airSpeed > 0) {
+                    resetInAir();
+                } else {
+                    airSpeed = fallSpeedAfterCollision;
+                }
+                updateXPos(xSpeed);
+            }
+        }
+        else {
+            updateXPos(xSpeed);
+        }
+        isMoving = true;
+    }
+
+    private void jump() {
+        if(inAir) return;
+
+        inAir = true;
+        airSpeed = jumpSpeed;
+    }
+
+    private void resetInAir() {
+        inAir = false;
+        airSpeed = 0;
+    }
+
+    private void updateXPos(float xSpeed) {
+        if(CanMoveHere(hitBox.x + xSpeed, hitBox.y, hitBox.width, hitBox.height, levelData)) {
+            hitBox.x += xSpeed;
+        } else {
+            hitBox.x = GetEntityXPosNextToWall(hitBox, xSpeed);
+        }
     }
 
     private void loadAnimations() {
@@ -113,6 +162,9 @@ public class Player extends Entity {
 
     public void loadLevelData(int[][] levelData) {
         this.levelData = levelData;
+        if(!IsEntityOnFloor(hitBox, levelData)) {
+            inAir = true;
+        }
     }
 
     public void resetDirBooleans() {
@@ -156,5 +208,9 @@ public class Player extends Entity {
 
     public void setDown(boolean down) {
         this.down = down;
+    }
+
+    public void setJump(boolean jump) {
+        this.jump = jump;
     }
 }
--- src/main/utils/HelpMethods.java
+++ src/main/utils/HelpMethods.java
@@ -2,6 +2,8 @@ package src.main.utils;
 
 import src.main.Game;
 
+import java.awt.geom.Rectangle2D;
+
 public class HelpMethods {
 
     public static boolean CanMoveHere(float x, float y, float width, float height, int[][] levelData){
@@ -27,5 +29,41 @@ public class HelpMethods {
         if (value >= 48 || value < 0 || value != 11)
             return true;
         return false;
+    }
+
+    public static float GetEntityXPosNextToWall(Rectangle2D.Float hitBox, float xSpeed) {
+        int currentTile = (int)(hitBox.x / Game.TILES_SIZE);
+        if(xSpeed > 0) {
+            //right
+            int tileXPos = currentTile * Game.TILES_SIZE;
+            int xOffset = (int)(Game.TILES_SIZE - hitBox.width);
+
+            return tileXPos + xOffset - 1;
+        } else {
+            //left
+            return currentTile * Game.TILES_SIZE;
+        }
+    }
+
+    public static float GetEntityYPosUnderRoofOrAboveFloor(Rectangle2D.Float hitBox, float airSpeed) {
+        int currentTile = (int)(hitBox.y / Game.TILES_SIZE);
+        if(airSpeed > 0) {
+            // Falling or touching ground
+            int tileYPos = currentTile * Game.TILES_SIZE;
+            int yOffset = (int)(Game.TILES_SIZE - hitBox.height);
+            return tileYPos + yOffset - 1;
+        } else {
+            // Jumping
+            return currentTile * Game.TILES_SIZE;
+        }
+    }
+
+    public static boolean IsEntityOnFloor(Rectangle2D.Float hitBox, int[][] levelData) {
+        // Check pixel below bottom left and bottom right corners
+
+        if(!isSolid(hitBox.x, hitBox.y + hitBox.height + 1, levelData))
+            if(!isSolid(hitBox.x + hitBox.width, hitBox.y + hitBox.height + 1, levelData))
+                return false;
+        return true;
     }
 }
